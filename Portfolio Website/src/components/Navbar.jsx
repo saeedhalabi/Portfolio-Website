c6b30b865adc7e2dcbe01c658aef6aa3387066d9
saeedhalabi <saeedhalabi80@gmail.com>
2024-05-16 14:07:49 +0300

    Add style for

--- Portfolio Website/src/components/Navbar.jsx
+++ Portfolio Website/src/components/Navbar.jsx
@@ -4,9 +4,32 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <>
-    
+      <nav className="navbar-wrapper">
+        <div className="content">
+          <h5>Saeed.</h5>
+          <ul>
+            <li>
+              <a href="#" className="nav-item">
+                Home
+              </a>
+              <a href="#" className="nav-item">
+                About
+              </a>
+              <a href="#" className="nav-item">
+                Resume
+              </a>
+              <a href="#" className="nav-item">
+                Projects
+              </a>
+              <a href="#" className="nav-item">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
