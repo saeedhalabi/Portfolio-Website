70bf95de85067b38e1d683ca15b39feb85479ef0
saeedhalabi <saeedhalabi80@gmail.com>
2024-05-17 17:49:10 +0300

    Contact section and added emailjs

--- Portfolio Website/src/App.jsx
+++ Portfolio Website/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import "./App.scss";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
---
+++ Portfolio Website/src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contact.scss";
+const Contact = () => {
+  return (
+    <>
+      <div className="contact">
+        <h1>
+          <span>Contact</span> Me
+        </h1>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Contact;
